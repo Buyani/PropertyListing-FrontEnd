56ec9a5cd838653614e8e0ddc5c8e97cc2691bb4
Buyani Mhlongo <buyanimhlongo@gmail.com>
2022-02-28 16:38:13 +0200

    page routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { RegisterComponent } from './account/register/register.component';
 import { HomeComponent } from './properties/home/home.component';
 import { HomesListPageComponent } from './properties/homeslistpage/homeslistpage.component';
 import { ListPropertyPageComponent } from './properties/listpropertypage/listpropertypage.component';
 
 const routes: Routes = [
-  {path:'/', component:HomeComponent },
-  { path:'/properties' ,component: HomesListPageComponent},
+  {path:'', component:HomeComponent },
+  { path:'properties' ,component: HomesListPageComponent},
   //add a guard here to check if user logged in 
-  {path :'/listproperty',component: ListPropertyPageComponent}
+  {path :'listproperty',component: ListPropertyPageComponent},
+  {path :'register',component: RegisterComponent},
+  {path :'login',component: RegisterComponent}
 ];
 
 @NgModule({
--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,3 +1,8 @@
 export class User{
-
+    //id: number;
+   // username: string;
+//password: string;
+  //  foreName: string;
+  //  surName: string;
+ //   token?: string;
 }
