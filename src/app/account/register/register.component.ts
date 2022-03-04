466738e8f0cb41a901e87384fb9cf80f74712ec3
Buyani Mhlongo <buyanimhlongo@gmail.com>
2022-03-04 11:43:40 +0200

    account dropdown

--- src/app/account/register/register.component.ts
+++ src/app/account/register/register.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { AbstractControl, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 import { LoaderHelper } from 'src/app/helpers/loader.helper';
 import { NotificationHelper } from 'src/app/helpers/notifications.helper';
@@ -7,76 +12,122 @@ import Validation from 'src/app/helpers/validators.helper';
 import { User } from 'src/app/models/user.model';
 import { UserManager } from 'src/app/services/account.service';
 
-function MatchPassword(control: AbstractControl) {
-  let password = control.get('password').value;
-  let confirmPassword = control.get('confirm_password').value;
-
-  if (password != confirmPassword) {
-    return { ConfirmPassword: true };
-  } else {
-      return null
+function passwordMatcher(
+  c: AbstractControl
+): { [key: string]: boolean } | null {
+  const password = c.get('password');
+  const confirmpassword = c.get('confirmpassword');
+  if (password.pristine || confirmpassword.pristine) {
+    return null;
   }
+
+  if (password.value === confirmpassword.value) {
+    return null;
+  }
+
+  return { match: true };
 }
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  styleUrls: ['./register.component.css'],
 })
 export class RegisterComponent implements OnInit {
-
   registerForm: FormGroup;
+  packageForm: FormGroup;
   loading = false;
   submitted = false;
   user: User;
   errorMessage: string = '';
 
   constructor(
-    private formBuilder: FormBuilder,
+    private fb: FormBuilder,
     private router: Router,
     private userService: UserManager,
     private notificationHelper: NotificationHelper,
-    private loaderHelper: LoaderHelper) { }
+    private loaderHelper: LoaderHelper
+  ) {}
 
   ngOnInit() {
-    this.registerForm = this.formBuilder.group({
-      forename: ['', [Validators.required, Validators.maxLength(100), Validators.minLength(1)]],
-      surname: ['', Validators.required, Validators.maxLength(100), Validators.minLength(3)],
-      email: ['', [Validators.required, Validators.maxLength(100), Validators.minLength(6), Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)]],
-      confirmpassword: ['', [Validators.required, Validators.minLength(6), Validators.pattern(/[!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/)]]
-    },{validator:MatchPassword});
+    this.registerForm = this.fb.group(
+      {
+        forename: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(100),
+            Validators.minLength(1),
+            Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)
+          ],
+        ],
+        surname: [
+          '',[
+          Validators.required,
+          Validators.maxLength(100),
+          Validators.minLength(3),
+          Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)
+          ],
+        ],
+        email: [
+          '',
+          [
+            Validators.required,
+            Validators.maxLength(100),
+            Validators.minLength(6),
+            Validators.email,
+          ],
+        ],
+        password: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern("^[a-zA-Z0-9 ]+$"),
+          ],
+        ],
+        confirmpassword: [
+          '',
+          [
+            Validators.required,
+            Validators.minLength(6),
+            Validators.pattern("^[a-zA-Z0-9 ]+$"),
+          ],
+        ],
+      },
+      { validator: [Validation.match('password','confirmpassword') ]}
+    );
   }
 
-  
-
-
   // convenience getter for easy access to form fields
-  get f() { return this.registerForm.controls; }
+  get f() {
+    return this.registerForm.controls;
+  }
 
   onSubmit() {
     this.submitted = true;
 
     // stop here if form is invalid
     if (this.registerForm.invalid) {
-      this.errorMessage = "Error occured make sure all fields are correct";
+      this.errorMessage = 'Error occured make sure all fields are correct';
       return;
+    } else {
+      //map form values to object values
+      const s = { ...this.user, ...this.registerForm.value };
+      //show loader while posting to api
+      this.loaderHelper.showLoader();
+      this.loading = true;
+
+      //call register service to save new user
+      this.userService.register(s).subscribe({
+        next: () => this.continue(),
+        error: (err) => (this.errorMessage = err),
+      });
+
+      this.userService.getUsers().subscribe({
+        next: (data) => console.log(data),
+      });
     }
-    //map form values to object values 
-    const s = { ...this.user, ...this.registerForm.value };
-    //show loader while posting to api
-    this.loaderHelper.showLoader();
-    this.loading = true;
-
-    //call register service to save new user
-    this.userService.register(s).subscribe({
-      next: () => this.continue(),
-      error: err => this.errorMessage = err
-    })
-
-    this.userService.getUsers().subscribe({
-      next: (data) => console.log(data)
-    })
   }
 
   continue(): void {
--- src/app/helpers/validators.helper.ts
+++ src/app/helpers/validators.helper.ts
@@ -2,6 +2,8 @@ import { AbstractControl, ValidatorFn } from '@angular/forms';
 
 
 export default class Validation {
+
+
   static match(controlName: string, checkControlName: string): ValidatorFn {
     return (controls: AbstractControl) => {
       const control = controls.get(controlName);
@@ -10,6 +12,7 @@ export default class Validation {
         return null;
       }
       if (control?.value !== checkControl?.value) {
+        
         controls.get(checkControlName)?.setErrors({ matching: true });
         return { matching: true };
       } else {
