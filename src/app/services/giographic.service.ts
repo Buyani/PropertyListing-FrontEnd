0d829ed316f65d59243aea02445e2a8c15749705
Buyani Mhlongo <buyanimhlongo@gmail.com>
2022-03-08 16:29:47 +0200

    cities and provinces data

--- src/app/services/giographic.service.ts
+++ src/app/services/giographic.service.ts
@@ -17,53 +17,53 @@ export class GeoGraphicService{
 
         ];
       }
-      
+
       getCities() {
        return [
-        //Gauteng
-        {"id":1,"province_id":1,"Name":"Alberton"},
-        {"id":2,"province_id":1,"Name":"Benoni"},
-        {"id":3,"province_id":1,"Name":"Boksburg"},
+        //Free State
+        {"id":1,"province_id":1,"Name":"Harrismith"},
+        {"id":2,"province_id":1,"Name":"Welkom"},
+        {"id":3,"province_id":1,"Name":"Bethlehem"},
 
-
+        //Eastern Cape
         {"id":4,"province_id":2,"Name":"Elizabeth"},
         {"id":5,"province_id":2,"Name":"Grahamstown"},
         {"id":6,"province_id":2,"Name":"Mthatha"},
 
+        //Gauteng
+        {"id":7,"province_id":3,"Name":"Sandton"},
+        {"id":8,"province_id":3,"Name":"Soweto"},
+        {"id":9,"province_id":3,"Name":"Mshongo"},
 
-        {"id":7,"province_id":3,"Name":"Alberton"},
-        {"id":8,"province_id":3,"Name":"Alberton"},
-        {"id":9,"province_id":3,"Name":"Alberton"},
+        //Limpopo
+        {"id":10,"province_id":4,"Name":"Bela-Bela"},
+        {"id":11,"province_id":4,"Name":"Lephalale"},
+        {"id":12,"province_id":4,"Name":"Makhado"},
 
+        //Mpumalanga
+        {"id":13,"province_id":5,"Name":"Parktown"},
+        {"id":14,"province_id":5,"Name":"Parklands"},
+        {"id":15,"province_id":5,"Name":"Ormonde"},
 
-        {"id":10,"province_id":4,"Name":"Alberton"},
-        {"id":11,"province_id":4,"Name":"Alberton"},
-        {"id":12,"province_id":4,"Name":"Alberton"},
+        //Northern Cape
+        {"id":16,"province_id":6,"Name":" Jan Kempdorp"},
+        {"id":17,"province_id":6,"Name":"Hartswater"},
+        {"id":18,"province_id":6,"Name":"Kimberley"},
 
+        //North West
+        {"id":19,"province_id":7,"Name":"Hartebeespoort"},
+        {"id":20,"province_id":7,"Name":"Rustenburg"},
+        {"id":21,"province_id":7,"Name":"Hartebeespoort"},
 
-        {"id":13,"province_id":5,"Name":"Alberton"},
-        {"id":14,"province_id":5,"Name":"Alberton"},
-        {"id":15,"province_id":5,"Name":"Alberton"},
+        //KwaZulu Natal
+        {"id":22,"province_id":8,"Name":"Durban"},
+        {"id":23,"province_id":8,"Name":"Richards Bay"},
+        {"id":24,"province_id":8,"Name":"Newcastle"},
 
-
-        {"id":16,"province_id":6,"Name":"Alberton"},
-        {"id":17,"province_id":6,"Name":"Alberton"},
-        {"id":18,"province_id":6,"Name":"Alberton"},
-
-
-        {"id":19,"province_id":7,"Name":"Alberton"},
-        {"id":20,"province_id":7,"Name":"Alberton"},
-        {"id":21,"province_id":7,"Name":"Alberton"},
-
-
-        {"id":22,"province_id":8,"Name":"Alberton"},
-        {"id":23,"province_id":8,"Name":"Alberton"},
-        {"id":24,"province_id":8,"Name":"Alberton"},
-
-
-        {"id":25,"province_id":9,"Name":"Alberton"},
-        {"id":26,"province_id":9,"Name":"Alberton"},
-        {"id":27,"province_id":9,"Name":"Alberton"},
+        //Western Cape
+        {"id":25,"province_id":9,"Name":"Barrydale"},
+        {"id":26,"province_id":9,"Name":"Bredasdorp"},
+        {"id":27,"province_id":9,"Name":"Cape Town "},
         ];
       }
 }
