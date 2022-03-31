141dfb0176d5101e5436b03e0addd4591d2bd9b2
Buyani Mhlongo <buyanimhlongo@gmail.com>
2022-03-31 12:50:00 +0200

    user filter by surname

--- src/app/properties/user-management/user-management.component.ts
+++ src/app/properties/user-management/user-management.component.ts
@@ -12,6 +12,7 @@ import { UserManager } from 'src/app/services/account.service';
 export class UserManagementComponent implements OnInit {
 
   users:User[];
+  filteredUsers:User[];
 
   constructor(private loaderHelper:LoaderHelper,
     private userManager:UserManager,
@@ -22,12 +23,26 @@ export class UserManagementComponent implements OnInit {
     this.getUsers();
   }
 
+  private _searchTerm: string;
 
+  get searchTerm(): string {
+    return this._searchTerm;
+  }
+  set searchTerm(value: string) {
+    this._searchTerm = value;
+    this.filteredUsers = this.filteruserBySurname(value);
+  }
+
+  //sesrch user by surname
+  filteruserBySurname(surname:string){
+    return this.users.filter((user)=> user.surname.toLowerCase().indexOf(surname.toLowerCase())!==-1)
+  }
   //get a lsit of users
   getUsers():void{
     this.userManager.getUsers().subscribe({
       next:data=>{
         this.users=data;
+        this.filteredUsers=this.users;
         this.loaderHelper.hideLoader();
       },
       error:err=>{
